cd58130005ced46c162e0513082f3ac096699fb2
khaleedbnwaleed <khaleedbnwaleed@gmail.com>
2025-09-15 19:08:31 +0100

    partners

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -60,14 +60,14 @@ export default function PartnersPage() {
       </div>
       <div>
         <div className="w-16 h-16 rounded-full flex items-center justify-center mx-auto mb-4">
-          <img src="/mm.jpg" alt="States Covered" className="w-12 h-12" />
+          <img src="/mm.jpg" alt="States Covered" className="w-20 h-20" />
         </div>
         <div className="text-3xl font-bold text-primary mb-2"></div>
         <div className="text-muted-foreground">HAMUNA COURIER AND LOGISTIC SERVICES</div>
       </div>
       <div>
         <div className="w-16 h-16 rounded-full flex items-center justify-center mx-auto mb-4">
-          <img src="/kano.jpg" alt="Years Partnership" className="w-25 h-25" />
+          <img src="/kano.jpg" alt="Years Partnership" className="w-20 h-20" />
         </div>
         <div className="text-3xl font-bold text-primary mb-2"></div>
         <div className="text-muted-foreground">KANO STATE MINISTRY OF EDUCATION</div>
